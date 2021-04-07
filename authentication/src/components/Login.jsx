b3934a0edf07f932a1f53b8a11bdeca9330b4300
HRUSHI003 <hrushibadgujar003@gmail.com>
2021-04-07 13:48:36 +0530

    LoginSignup UI changed

---
+++ authentication/src/components/Login.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import {Row, Col, Form, FormControl, FormGroup, FormLabel, FormCheck, Button } from 'react-bootstrap';
+
+class Login extends Component {
+    render() {
+        return (
+            <Form>
+                <h3>Log in</h3>
+
+                <FormGroup controlId="formBasicEmail">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl type ="email" placeholder="Enter email"></FormControl>
+                </FormGroup>
+
+                <FormGroup controlId="formBasicPassword">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl type ="password" placeholder="Enter password"></FormControl>
+                </FormGroup>
+
+                <FormGroup controlId="formBasicCheckbox">
+                    <FormCheck type="checkbox" label="Remember me" />
+                </FormGroup>
+
+                <Button variant="primary" type="submit" block>Submit</Button>
+
+                <Row>
+                    <Col className="new-user">
+                        New User?<a href="#">Create account</a>
+                    </Col>    
+                    <Col className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </Col>
+                </Row>              
+
+            </Form>
+        );
+    }
+}
+
+export default Login;
--- authentication/src/index.js
+++ authentication/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {BrowserRouter} from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <App/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
